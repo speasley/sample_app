4ec4cdbd9a8391935b9cea9030d29d04d291da3c
Stephen Peasley <s@stephenpeasley.com>
2014-02-02 21:59:01 -0700

    Complete Chapter 8 work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it { should be_valid }
@@ -48,6 +49,11 @@ describe User do
         @user.email = invalid_address
         expect(@user).not_to be_valid
       end
+    end
+    
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token){ should_not be_blank }
     end
   end
   
