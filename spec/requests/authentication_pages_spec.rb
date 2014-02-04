ba494fd9d0eff4891da75300c9434d8f2d84cc39
Stephen Peasley <s@stephenpeasley.com>
2014-02-03 22:39:33 -0700

    Complete work up to Chapter 9.1 inclusive

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,4 +36,14 @@ describe "Authentication" do
     end
   end
   
+  describe "with valid information" do
+    let(:user){ FactoryGirl.create(:user) }
+    before { sign_in user }
+    it { should have_selector('title', text: user.name) }
+    it { should have_link('Profile', href: user_path(user)) }
+    it { should have_link('Settings', href: edit_user_path(user)) }
+    it { should have_link('Sign out', href: signout_path) }
+    it { should_not have_link('Sign in', href: signin_path) }
+  end
+  
 end
